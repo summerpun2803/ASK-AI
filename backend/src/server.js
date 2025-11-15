027a83fcd91bca5a252a0ced6af877aface0b2e0
Samarpan <mastersam2003@gmail.com>
2025-11-15 17:44:20 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ await connectToMongo(); // important: ensure DB connected before starting
 const app = express();
 app.use(express.json({ limit: "2mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: [FRONTEND_ORIGIN, "http://localhost:3000", "*"], credentials: true }));
+app.use(cors({ origin: [FRONTEND_ORIGIN, "http://localhost:3000", "*"] }));
 
 // mounted routers
 import authRoutes from "./routes/auth.js";
